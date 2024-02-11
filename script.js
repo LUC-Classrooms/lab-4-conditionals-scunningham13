b9cc4b0e4f122b224fe8829b1e60e3bf2eb6266d
scunningham13 <156953717+scunningham13@users.noreply.github.com>
2024-02-11 20:22:53 +0000

    Lab Four Complete

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ function draw() {
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    
+    fill (50, 85, 20);
+
   }
   else { 
     // otherwise do this:
-    
+    fill (110, 60, 80);
   }
   ellipse(width/2, height/2, 100, 100);
 }
